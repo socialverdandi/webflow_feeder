9fddc40f8e0172f8a77d30a8a3175bde58dfacb1
deemarc <marteleus@gmail.com>
2021-05-31 01:24:26 +0700

    using config from environment

--- config.py
+++ config.py
@@ -3,3 +3,20 @@ import logging
 import sys
 
     
+key_list = [
+    'FOOTBALL_API_URL',
+    'FOOTBALL_API_TOKEN',
+    'LEAGUE_ID',
+    'WEBFLOW_API_URL',
+    'WEBFLOW_API_VERSION',
+    'WEBFLOW_API_TOKEN',
+    'MATCHES_ON_DAY_ID',
+    'MATCHES_IN_DAYS_ID',
+    'MATCHES_IN_MONTHS_ID',
+    'SEASON'
+
+]
+config = {}
+for key in key_list:
+    config[key] = os.environ.get(key) 
+    logging.info("key:{} hace value:{}".format(key, config[key]))
